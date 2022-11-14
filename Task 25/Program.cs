bc815d4665a980269f88dedde89b2909e638d686
Burundukov Denis <burundukov.d@yandex.ru>
2022-11-14 23:03:40 +0500

    Task 25 completed

---
+++ Task 25/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 25: Напишите программу, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B (Math.Pow НЕ использовать)
+
+// 3, 5 -> 243 (3⁵)
+// 0, 0 - >1
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число А для возведения в натуральную степень B");
+int A = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите степень B");
+int B = int.Parse(Console.ReadLine());
+
+void Degree()
+{
+    int deg = 1;
+    for(int i = 0; i < B; i++)
+    {
+        deg = A * deg;
+    }
+Console.WriteLine((deg));
+}
+
+Degree();
