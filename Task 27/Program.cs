494b3ff3d47d75c728189fa3f18d7dd1fe97d970
Burundukov Denis <burundukov.d@yandex.ru>
2022-11-15 16:54:03 +0500

    Task 27 completed

---
+++ Task 27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int SummOfNumbers(int num)
+{   int res = 0;
+    while (num > 0)
+    {
+        res += num % 10;
+        num = num/10;
+    
+    }
+    return res;
+}
+
+Console.WriteLine("Введите целое число");
+int number =  Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Сумма числ в числе {SummOfNumbers(number)}");
