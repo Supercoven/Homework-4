7b73fd05b899f0f91543a3179fedb0a95cdeea95
Burundukov Denis <burundukov.d@yandex.ru>
2022-11-17 19:14:17 +0500

    Task 4 incomplete

---
+++ Task 29/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая создает массив заданного пользователем размера, заполняет массив элементами от 1 до 99 и выводит их на экран.
+
+int [] UserCreateArray (int n)
+{
+    Console.WriteLine("введите размер массива");
+    n = int.Parse(Console.ReadLine());
+    int [] arr = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = new Random().Next(1,99);
+    }
+    return arr;
+}
+
+void PrintArray(int [] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"Полученный массив: [{arr[i]}]");
+    }
+}
